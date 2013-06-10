9d351f07fb1a14d2cbb8d6f8af98e48c4b57c1f4
vlad101 <vladimir.efros@gmail.com>
2013-06-10 10:20:30 -0400

    First Commit
    Created Homepwner project.

---
+++ Homepwner/Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Vladimir Efros on 6/10/13.
+//  Copyright (c) 2013 Vladimir Efros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Vladimir Efros on 6/10/13.
+//  Copyright (c) 2013 Vladimir Efros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
